cb1d174dde69865a95f46b10d99fb838579a44ee
Brandon Ngo <okibrandon@hotmail.com>
2021-09-06 21:18:44 -0400

    blinking cursor navigation implemented

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import Layout from '../components/layout'
+import "../components/animations.css"
 import { Link } from 'gatsby'
 
 // styles
@@ -12,6 +13,7 @@ const IndexPage = () => {
       <p>Brandon Ngo</p>
       <p>Software Developer. Looking for 2021 internships.</p>{"\n"}
       <p>Check out my projects here <Link to=""></Link></p>{"\n"}
+      <p>PS C:\brandon{"\>"} <span class="blinkingCursor">|</span></p>
     </Layout>
   )
 }
