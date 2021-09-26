e5664832f31668a2ce90493d9c184f7426edd065
Brandon Ngo <okibrandon@hotmail.com>
2021-09-26 01:22:45 -0400

    Fixed contact form submit button to resize

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,37 +1,43 @@
-import * as React from 'react'
-import FormOption from './FormOption'
+import * as React from "react";
+import FormOption from "./FormOption";
 
 const commentBox = {
   width: "70%",
   height: "30vh",
-}
+};
 
 const submitBtn = {
   width: "15%",
-  height: "2vh",
-  fontSize: "14px",
+  fontSize: "2vh",
+  padding: "10px 0",
   fontFamily: "Consolas",
   textAlign: "center",
   backgroundColor: "dodgerblue",
   outline: "none",
   color: "white",
-  border: "none"
-}
+  border: "none",
+};
 
-const handleClick = () => {
-  
-}
+const handleClick = () => {};
 
 const ContactForm = () => {
-
   return (
     <div>
-      <FormOption inputType="name" label="Name:"/>
-      <FormOption inputType="email" label="Email:"/>
-      <FormOption inputType="comment" label="Message:" inputStyle={commentBox}/>
-      <input type="submit" value="Send" onClick={e => handleClick()} style={submitBtn}/>
+      <FormOption inputType="name" label="Name:" />
+      <FormOption inputType="email" label="Email:" />
+      <FormOption
+        inputType="comment"
+        label="Message:"
+        inputStyle={commentBox}
+      />
+      <input
+        type="submit"
+        value="Send"
+        onClick={(e) => handleClick()}
+        style={submitBtn}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default ContactForm
+export default ContactForm;
